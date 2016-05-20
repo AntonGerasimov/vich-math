e521f5140684e970205fcd84c6a97985ae7dff97
AntonGerasimov <antoshkasg@gmail.com>
2016-05-20 20:27:37 +0300

    first parabolic commit

---
+++ giper/main.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+double func(double x, double y){
+	return sin(x)+sin(y);
+}
+void solver(){
+
+}
+int main(){
+	printf("\n");
+	solver();
+	return 0;
+}
